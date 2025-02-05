47ca0d521d4fa402e41756ff529b0a4e236569fc
SreDeva <130994461+SreDeva@users.noreply.github.com>
2025-02-05 21:28:25 +0530

    solidity final

--- solidity/ignition/modules/Lock.js
+++ solidity/ignition/modules/Lock.js
@@ -4,7 +4,7 @@ const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
 module.exports = buildModule("VerifierContract", (m) => {
 
   // Deploy the VerifierContract (Implementation)
-  const Ex = m.contract("VerifierContract");
+  const VerifierContract = m.contract("VerifierContract");
 
   
   console.log("VerifierContract deployed at:", VerifierContract.address);
